c7bf2a7b691684d029d4b7f0f787020b79d89546
ItaloBasilio <italobasiliodasilva@gmail.com>
2024-02-16 22:43:37 -0300

    changes

--- client/js/scripts.js
+++ client/js/scripts.js
@@ -90,7 +90,7 @@ const handleLogin = (event) =>{
     login.style.display = "none"
     chat.style.display = "flex"
 
-    websocket = new WebSocket("ws://localhost:8080")
+    websocket = new WebSocket("wss://chat-backend-q33p.onrender.com")
     websocket.onmessage = processMessage
 
 }
